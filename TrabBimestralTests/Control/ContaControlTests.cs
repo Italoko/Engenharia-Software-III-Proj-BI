102c480720bd29266d5503bc7d9ba4100e8b5653
ITALO PIOVAN <italo_piovan@hotmail.com>
2021-09-15 21:23:31 -0300

    test sem gitignore

---
+++ TrabBimestralTests/Control/ContaControlTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrabBimestral.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabBimestral.Models;
+
+namespace TrabBimestral.Control.Tests
+{
+    [TestClass()]
+    public class ContaControlTests
+    {
+        bool sucesso = false;
+        string msg = "";
+        Conta conta;
+        ContaControl contaCtrl = ContaControl.getInstance();
+        
+
+        [TestMethod()]
+        public void GravarTest()
+        {
+            //DateTime dt = new DateTime(2021, 10, 05);
+            //(conta, sucesso, msg) = contaCtrl.Gravar(4, "SERVIÇO Y",dt,2500);
+            string filtro = "";
+            IEnumerable<Conta> cts;
+            (cts,msg) = contaCtrl.ObterTodos("x");
+
+            if (cts.Count() > 0)
+                sucesso = true;
+
+            Assert.IsTrue(sucesso);
+            if (sucesso)
+                sucesso = false;
+        }
+    }
+}
