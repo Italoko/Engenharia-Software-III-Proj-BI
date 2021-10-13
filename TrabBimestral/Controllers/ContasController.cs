730de43145ec0ec925cd4f7a572efd9da67a6895
barbaralexandre <62961696+barbaralexandre@users.noreply.github.com>
2021-10-13 20:37:46 -0300

    telas de contas

---
+++ TrabBimestral/Controllers/ContasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabBimestral.Controllers
+{
+    public class ContasController : Controller
+    {
+        public IActionResult ContasAPagar()
+        {
+            return View();
+        }
+
+        public IActionResult ContasAReceber()
+        {
+            return View();
+        }
+    }
+}
